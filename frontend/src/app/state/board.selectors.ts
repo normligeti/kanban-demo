97581ba97ed9ffca4a975603c8322575f85932de
Norman_Ligeti <n.ligeti@gmail.com>
2026-02-22 20:14:29 +0100

    repo reset

--- frontend/src/app/state/board.selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { BoardState } from './board.models';
-
-export const selectBoardState = createFeatureSelector<BoardState>('board');
-
-export const selectBoard = createSelector(selectBoardState, (state) => state.board);
-export const selectBoardLoading = createSelector(selectBoardState, (state) => state.loading);
-export const selectBoardError = createSelector(selectBoardState, (state) => state.error);
